aec8dc583ca7e50df692f55b42b322b587991538
qzzhv <30725461+qzzhv@users.noreply.github.com>
2022-03-13 23:28:11 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -12,5 +12,5 @@ with open(Path(DOMAINS_PATH), encoding="utf-8") as inp, \
     
     domains = (domain.strip() for domain in inp if len(domain.strip()) > 0 and not domain.startswith("#"))
     ips = chain(*((ai[-1][0] for ai in getaddrinfo(domain, 0, 0, 0, 0) if "." in ai[-1][0]) for domain in domains))
-    rules = (f"ROUTE ADD {ip} MASK 255.255.255.255 {GATEWAY_IP}" for ip in ips)
+    rules = (f"ROUTE ADD {ip} MASK 255.255.255.255 {GATEWAY_IP}" for ip in sorted(ips))
     print(*rules, sep="\n", file=out)
